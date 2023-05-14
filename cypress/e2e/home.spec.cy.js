21fdf6f2ff4fb91048854ba34de8f41f88c0db7e
josdem <joseluis.delacruz@gmail.com>
2023-05-14 10:13:58 -0400

    Deleting unused import

--- cypress/e2e/home.spec.cy.js
+++ cypress/e2e/home.spec.cy.js
@@ -1,8 +1,14 @@
 /// <reference types="cypress" />
 
 describe("Loading home page", () => {
+  before(function () {
+    cy.fixture("test").then((data) => {
+      this.data = data
+    })
+  })
+
   it("validates page title", function () {
     cy.visit(this.data.vetlogUrl)
-    
+    cy.title().should('eq', 'Vetlog')
   })
 })
